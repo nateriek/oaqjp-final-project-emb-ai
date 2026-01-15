229036c027327f22c438c838f623083da25bf75c
nateriek <nateriek98@gmail.com>
2026-01-15 15:35:27 -0500

    updated emotion_detection formatting

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,12 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     my_obj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=my_obj, headers=headers)
-    return response.text
+    response_dict = json.loads(response.text)
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    return emotions
